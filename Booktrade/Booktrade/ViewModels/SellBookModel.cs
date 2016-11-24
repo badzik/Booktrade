0448883303bdeb8b995c626528e2e1afaf64849c
Tomasz Jurkowski <jurek993@outlook.com>
2016-11-24 13:35:08 +0100

    adding preview view uploaded photo

--- Booktrade/Booktrade/ViewModels/SellBookModel.cs
+++ Booktrade/Booktrade/ViewModels/SellBookModel.cs
@@ -10,17 +10,25 @@ namespace Booktrade.ViewModels
     public class SellBookModel
     {
         [Required]
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
         [Required]
+        [Display(Name = "Opis")]
         public string Description { get; set; }
         [Required]
+        [Display(Name = "Gatunek")]
         public string Genre { get; set; }
         [Required]
+        [Display(Name = "Autor")]
         public string Author { get; set; }
+        [Display(Name = "Data wydania")]
         public DateTime PublicationDate { get; set; }
+        [Display(Name = "Wydawca")]
         public string Publisher { get; set; }
+        [Display(Name = "Chęć wymiany")]
         public bool Changeable { get; set; }
         [Required]
+        [Display(Name = "Cena")]
         public float Price { get; set; }
         public HttpPostedFileBase BookImage { get; set; }
 
