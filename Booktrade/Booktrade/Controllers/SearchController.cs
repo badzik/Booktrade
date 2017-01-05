9636f3bd13e46016aad7d05c47b4e72b58b73dc4
Bartosz Sosnowski <bartosz-sosnowski@o2.pl>
2017-01-05 15:13:50 +0100

    Added Search View

---
+++ Booktrade/Booktrade/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Booktrade.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Booktrade.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult SearchWindow(string category,string phrase)
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult SearchWindow(SearchModel model)
+        {
+            return View(); //PartialView
+        }
+    }
+}
---
+++ Booktrade/Booktrade/ViewModels/SearchModel.cs
@@ -0,0 +1,25 @@
+﻿using Booktrade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booktrade.ViewModels
+{
+    public class SearchModel
+    {
+        public string Phrase { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public bool ForExchange { get; set; }
+        public bool ForSell { get; set; }
+        public float PriceFrom { get; set; }
+        public float PriceTo { get; set; }
+        public string Publisher { get; set; }
+        public int PublicationYear { get; set; }
+        public string SortBy { get; set; }
+        public string Category { get; set; }
+        public int CurrentPage { get; set; }
+        public ICollection<Book> Results { get; set; }
+    }
+}
